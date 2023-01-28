0f1a0eddb9e3e579c9dccb8b87e9a285b75bae5b
Maryam-Sheikh-1000 <120272271+Maryam-Sheikh-1000@users.noreply.github.com>
2023-01-28 20:17:28 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,40 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+async function startFunc() {
+
+    const systemNum = Math.floor(Math.random() * 10);
+    const userNum = await inquirer.prompt([
+        {
+            type: "number",
+            name: "userGuess",
+            message: "Enter your number between 1-10:"
+        }
+    ])
+
+    const {userGuess} = userNum;
+
+    console.log("Your Guess: ", userGuess, "\nSystem's Guess: ", systemNum);
+
+    if(userGuess === systemNum){
+        console.log("Your Guess Is Correct \nYou Won!");
+    }
+    else {
+        console.log("Better Luck Next Time");
+    }
+}
+
+async function startAgain() {
+    do{
+        await startFunc();
+        var again = await inquirer
+        .prompt({
+            type: "input",
+            name: "restart",
+            message: "Do You Want to Continue? Press Y or N"
+        })
+    }
+    while ( again.restart == "Y" || again.restart == "y" || again.restart == "YES" || again.restart == "yes")
+}
+startAgain();
